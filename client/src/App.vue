556340dda00b104af1e0b8acfa48f4dac3530d32
carlottahillger <78755376+carlottahillger@users.noreply.github.com>
2022-10-05 17:22:38 +0200

    Add SIgn In and Sign Up page
    - create page for sign in
    - create page for sign up
    - add routes
    - add if statement to App.vue to check whether the user is logged in
    - Co-authored-by: Juan García Díaz <juangarc@student.chalmers.se>

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,35 +1,41 @@
 <template>
   <div id="app">
-    <menu-header/>
-    <div id="nav">.</div>
-    <div class="row">
-      <div class="col-sm-auto bg-light sticky-top" id="menu">
-        <h5>Menu</h5>
-        <p>Here we can have a picture of the exercise</p>
-        <div class="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
-          <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center align-items-center">
-            <li>
-              <router-link to="/">Home</router-link>
-            </li>
-            <li>
-              <router-link to="/user">Profile</router-link>
-            </li>
-            <li>
-              <router-link to="/exercises" >Exercises</router-link>
-            </li>
-            <li>
-              <router-link to="/saved-exercises" >Saved Exercises</router-link>
-            </li>
-            <li>
-              <router-link to="/body-map" >Body Map</router-link>
-            </li>
-          </ul>
+    <div v-if="isLoggedIn">
+      <menu-header/>
+      <div id="nav">.</div>
+      <div class="row">
+        <div class="col-sm-auto bg-light sticky-top" id="menu">
+          <h5>Menu</h5>
+          <p>Here we can have a picture of the exercise</p>
+          <div class="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
+            <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center align-items-center">
+              <li>
+                <router-link to="/">Home</router-link>
+              </li>
+              <li>
+                <router-link to="/user">Profile</router-link>
+              </li>
+              <li>
+                <router-link to="/exercises" >Exercises</router-link>
+              </li>
+              <li>
+                <router-link to="/saved-exercises" >Saved Exercises</router-link>
+              </li>
+              <li>
+                <router-link to="/body-map" >Body Map</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm">
+          <!-- Render the content of the current page view -->
+          <router-view/>
         </div>
       </div>
-      <div class="col-sm">
-        <!-- Render the content of the current page view -->
-        <router-view/>
-      </div>
+    </div>
+    <div v-else>
+      <nav><router-link to="/sign-in">Sign In</router-link> | <router-link to="/sign-up">Sign Up</router-link></nav>
+      <router-view/>
     </div>
   </div>
 </template>
@@ -41,6 +47,12 @@ export default {
   components: {
     // Define the name of the component here
     'menu-header': Header
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+      user: {}
+    }
   }
 }
 </script>
---
+++ client/src/views/SignIn.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+        <img src="@/assets/Logo.png" alt="logo" width="50%" />
+        <b-jumbotron header="Welcome back!" lead="Please sign in with you email address and your password.">
+            <br>
+            <input type="text" id="email" name="email" placeholder="Email" size="40"><br><br>
+            <input type="text" id="password" name="passwork" placeholder="Password" size="40"><br><br>
+            <b-button href="#" variant="info">Log in</b-button>
+        </b-jumbotron>
+    </div>
+</template>
