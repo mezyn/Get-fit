55cc94247de7a56c45b4b90bf8455515513e4bdc
Juan García Díaz <juandz2000@gmail.com>
2022-10-08 12:50:01 +0200

    Add homepage
    - description and images of nutrition, macro- and micronutrients

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,33 +1,51 @@
 <template>
   <div class="container">
-    <b-jumbotron header="Welcome!" lead="Are you ready to start your fitness journey with GET FIT?" id="background">
-      <b-button class="btn_message" variant="primary" v-on:click="getMessage()" >Get Message from Server</b-button>
-      <p>Message from the server:<br/>
-      {{ message }}</p>
-    </b-jumbotron>
+    <div class="row">
+      <div class="col">
+        <img src="@/assets/chicken_rice.jpg"  alt="profile" width="100%" />
+      </div>
+      <div class="col">
+        <h3 class="text">Nutrition</h3>
+        <p class="text"> Nutrition is the most important part of your journey to becoming healthy,
+          no amount of effort in workouts will compensate for an insufficient or unhealthy diet.
+          But dieting is simpler than most people think. To break down this complex topic we have to
+          understand that humans need enough calories, macro and micronutrients to properly function.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h3 class="text">Macronutrients</h3>
+        <p class="text">Carbohydrates: Source of energy and can be found in large quantities in common foods like rice, bread, potatoes and beans, among others.
+Proteins:. used to build muscles and organs, and to make hormones and antibodies. They can be found, among other foods, in meats, fishes and also in vegetable alternatives like tofu, beans, broccoli and spinach.
+Fats: a wildly misunderstood macronutrient, they provide energy, they help regulate body temperature and support cell functions. Although fats are a necessary macronutrient, not all fats are good, especially those in processed foods. Some healthy alternatives are olive oil, nuts, avocados and salmon.
+        </p>
+      </div>
+      <div class="col">
+        <img src="@/assets/macronutrients.png"  alt="profile" width="100%" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img src="@/assets/micronutrients.jpg"  alt="profile" width="100%" />
+      </div>
+      <div class="col">
+        <h3 class="text">Micronutrients</h3>
+        <p class="text"> Micronutrients can be divided into four groups: water-soluble vitamins, fat-soluble vitamins, macrominerals and trace minerals. In order to obtain enough micronutrients it is important to include fruits, vegetables and seeds into your diet.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { Api } from '@/Api'
 
 export default {
   name: 'home',
   data() {
     return {
       message: 'none'
-    }
-  },
-  methods: {
-    getMessage() {
-      Api.get('/')
-        .then(response => {
-          this.message = response.data.message
-        })
-        .catch(error => {
-          this.message = error
-        })
     }
   }
 }
@@ -40,5 +58,14 @@ export default {
 
 #background {
   background-color: white;
+}
+
+.text {
+  text-align: justify;
+  hyphens: auto;
+}
+
+.row {
+  padding: 2%;
 }
 </style>
