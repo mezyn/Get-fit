df4c18ddfdb097c9e7f3c0ae4424efc91f3980f2
mijin <mijin@student.chalmers.se>
2022-10-13 21:45:35 +0000

    Revert "Merge branch 'MS2-Frontend-Implementation' into 'master'"
    This reverts merge request !4

--- client/src/router.js
+++ client/src/router.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Exercises from './views/Exercises.vue'
-import Exercise from './views/Exercise.vue'
-import User from './views/User.vue'
-import SavedExercises from './views/SavedExercises.vue'
-import BodyMap from './views/BodyMap.vue'
-import SignIn from './views/SignIn.vue'
-import SignUp from './views/SignUp.vue'
-import ManageExercises from './views/ManageExercises.vue'
-import Muscle from './views/Muscle.vue'
 
 Vue.use(Router)
 
@@ -21,61 +12,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/exercises',
-      name: 'exercises',
-      component: Exercises
-    },
-    {
-      path: '/exercises/:id',
-      name: 'exercise',
-      component: Exercise
-    },
-    {
-      path: '/user',
-      name: 'user',
-      component: User
-    },
-    {
-      path: '/saved-exercises',
-      name: 'saved-exercises',
-      component: SavedExercises
-    },
-    {
-      path: '/body-map',
-      name: 'body-map',
-      component: BodyMap
-    },
-    {
-      path: '/sign-in',
-      name: 'sign-in',
-      component: SignIn
-    },
-    {
-      path: '/sign-up',
-      name: 'sign-up',
-      component: SignUp
-    },
-    {
-      path: '/manage-exercises',
-      name: 'manage-exercises',
-      component: ManageExercises
-    },
-    {
-      path: '/signin',
-      name: 'signin',
-      component: SignIn
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: SignUp
-    },
-    {
-      path: '/muscles/:id',
-      name: 'muscle',
-      component: Muscle
     }
   ]
 })
