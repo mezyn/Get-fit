4737b2cbc2b2f416904d420390098c32f3017de4
carlottahillger <78755376+carlottahillger@users.noreply.github.com>
2022-10-14 11:52:25 +0200

    Delete person icon

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="row">
-    <div class="col-6 col-md-4">
-      <router-link to="/user"><img src="@/assets/profile.png"  alt="profile" width="15%" /></router-link>
-    </div>
+    <div class="col-6 col-md-4"> </div>
     <div class="col-6 col-md-4"> </div>
     <div class="col-6 col-md-4">
-      <router-link to="/"><img src="@/assets/Logo.png" alt="logo" width="60%" /></router-link>
+      <router-link to="/"><img src="@/assets/Logo.png" alt="logo" width="100%" /></router-link>
     </div>
   </div>
 </template>
